ecb825fabec8891412c03c84eb919ba65d06de9a
infinitybrad <seoih415@gmail.com>
2019-05-21 20:25:36 +0900

    Styling Stack Navigators

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,12 @@
-export const BG_COLOR ="black";
-export const WHITE = "white";
+// export const BG_COLOR ="black";
+// export const TINT_COLOR = "white";
+// export const GREY_COLOR = "#bdc3c7";
+// export const INACTIVE_COLOR = "#7f8c8d";
+// //export const INACTIVE_COLOR = "red";
+// export const ACTIVE_COLOR = TINT_COLOR;
+
+export const BG_COLOR = "black";
+export const TINT_COLOR = "white";
 export const GREY_COLOR = "#bdc3c7";
 export const INACTIVE_COLOR = "#7f8c8d";
-//export const INACTIVE_COLOR = "red";
-export const ACTIVE_COLOR = WHITE;
+export const ACTIVE_COLOR = TINT_COLOR;
--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,7 +1,7 @@
 import {createStackNavigator,createAppContainer} from "react-navigation";
 import TabNavigation from "./TabNavigation";
 import DetailScreen from "../screens/Detail";
-
+import {headerStyles} from "./config";
 const MainNavigation = createStackNavigator(
 
     {
@@ -11,7 +11,12 @@ const MainNavigation = createStackNavigator(
                 header:null
             }
         },
-        Detail:DetailScreen
+        Detail:{
+            screen:DetailScreen,
+            navigationOptions:{
+                ...headerStyles
+            }
+        }
     },
     {}
 );
