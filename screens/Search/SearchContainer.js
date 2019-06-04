39fd64ab8623e7afac3d3b85c77528c992432f80
infinitybrad <seoih415@gmail.com>
2019-06-04 22:04:23 +0900

    Search Screen part One

---
+++ screens/Search/SearchContainer.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import SearchPresenter from "./SearchPresenter";
+
+export default class extends Component{
+
+    state ={
+        loading : false,
+        movieResults : null,
+        tvResults:null,
+        searchTerm:""
+    };
+
+    handleSearchUpdate = text =>{
+        this.setState({
+            searchTerm:text
+        });
+        //console.log(text);
+
+    };
+    render(){
+        const{loading,movieResults,tvResults,searchTerm}=this.state;
+
+        return(
+            <SearchPresenter 
+                loading={loading}
+                movieResults={movieResults}
+                tvResults={tvResults}
+                searchTerm={searchTerm}
+                handleSearchUpdate={this.handleSearchUpdate}
+            />
+
+        );
+
+    }
+}
